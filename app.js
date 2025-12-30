d93072ab9c981498f2569f7d8b63f6a1d8749ace
lehongnghia <nghia23112009lh@gmail.com>
2025-12-30 21:04:27 +0700

    Update giao dien + fix loi

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var fut = new Date("dec 28, 2025 23:05:50").getTime()
+var fut = new Date("jan 1, 2026 00:00:00").getTime()
 let x = setInterval(function stime() {
     var now = new Date().getTime()
     var D = fut - now
